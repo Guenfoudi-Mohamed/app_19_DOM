6bd09aca79902662a368da76c8c6b3ef09352193
GUENFOUDI MOHAMED <mohamed.guenfoudi@hotmail.com>
2022-06-24 14:49:37 +0100

    Update app_19.js

--- app_19.js
+++ app_19.js
@@ -6,17 +6,16 @@ const listMinute = document.querySelector('body #section .listTime .listMinute')
 const listAmPm = document.querySelector('body #section .listTime .listAmPm');
 const songAlarmMp3 = document.querySelector('body #section audio.soundAlarm');
 const imgAlarm = document.querySelector('body #section .img > img');
-// const arrSelect;
 const btnAlarm = document.querySelector('body #section > .btnAlarm');
 
-let hr = -1;
-let min = -1;
-const afterTimePhause = 1;
+let hourAlarm = -1;
+let minAlarm = -1;
+const afterTimePause = 2;
 let check = false;
 
-
-let arrSelect = document.querySelectorAll('body #section .listTime select');
+// event for btn 'Clear Alarm' & 'Set Alarm'
 btnAlarm.addEventListener('click',function(){
+    let arrSelect = document.querySelectorAll('body #section .listTime select');
     if(btnAlarm.value == 'Set Alarm'){
         let conteur = -1;
         for(let i = 0;i<arrSelect.length;i++){
@@ -37,10 +36,8 @@ btnAlarm.addEventListener('click',function(){
             }); 
             btnAlarm.value = 'Clear Alarm';
             btnAlarm.setAttribute('title','Clear Alarm'); 
-            hr = arrSelect[0].value;
-            min = (Number(arrSelect[1].value)+afterTimePhause);
-            console.log(min);
-            console.log(hr);
+            hourAlarm = arrSelect[0].value;
+            minAlarm = (Number(arrSelect[1].value)+afterTimePause) >= 60 ? ((Number(arrSelect[1].value)+afterTimePause) - 60) : (Number(arrSelect[1].value)+afterTimePause) ;
             check = true;
         }
     }
@@ -56,14 +53,18 @@ btnAlarm.addEventListener('click',function(){
         songAlarmMp3.pause();
         imgAlarm.classList.remove('imgAalarm');
         arrSelect.length = 0;
+        check = false;
     };
 });
 
 const stop = setInterval(Time,1000);
+// function time
 function Time(){
     const h2 = document.querySelector('body #section .time > h2');
     const date = new Date();
-    // date.setHours(13);                                   //test Hours
+    // date.setHours(00);                                   //test Hours
+    // date.setMinutes(40);                                 //test minutes
+    
     const hour = date.getHours()
     const minute = date.getMinutes();
     const second = date.getSeconds();
@@ -74,59 +75,41 @@ function Time(){
     else if(String(minute).length  > 1){h2.children[1].textContent = minute;}
     if(String(second).length == 1){h2.children[2].textContent = '0'+second;}
     else if(String(second).length  > 1){h2.children[2].textContent = second;}
-    if(hour > 12){
+    if(hour >= 12){
         h2.children[3].textContent = 'PM';
     } 
-    else if(hour <= 12){h2.children[3].textContent = 'AM'}
-    // 
-    function pauseAlarmAfterTime(h,m,Hou,Min){
-        if(arrSelect[2].value == 'AM'){
-            if( h == Hou && m == Min){
-                songAlarmMp3.pause();
-                imgAlarm.classList.remove('imgAalarm');
-                arrSelect.length = 0;
-                hr = -1;
-                min = -1;
-                check = false;
-            }
-        }
-        else if(arrSelect[2].value == 'PM'){
-            if( (Number(h)+12) == Hou && m == Min){
-                songAlarmMp3.pause();
-                imgAlarm.classList.remove('imgAalarm');
-                arrSelect.length = 0;
-                hr = -1;
-                min = -1;
-                check = false;
-            }
-        }
+    else if(hour < 12){h2.children[3].textContent = 'AM'}
+    // function for 
+    function pauseAlarmAfterTime(mAlarm,MinNow){
+        if( mAlarm == MinNow){
+            btnAlarm.click();
+        };
     }
     // 
     if(check == true){
-        // let arrSelect = document.querySelectorAll('body #section .listTime select');
+        let arrSelect = document.querySelectorAll('body #section .listTime select');
         if(arrSelect[2].value == 'AM'){
             if(hour == arrSelect[0].value && minute == arrSelect[1].value){
                 songAlarm();
-                pauseAlarmAfterTime(hr,min,hour,minute);
             }
         }
         else if(arrSelect[2].value == 'PM'){
-            if(hour == (Number(arrSelect[0].value)+12) && minute == arrSelect[1].value){
+            hourAlarm = Number(arrSelect[0].value)+12;
+            if(hour == hourAlarm && minute == arrSelect[1].value){
                 songAlarm();
-                pauseAlarmAfterTime(hr,min,hour,minute);
             } 
         }
-        
+        pauseAlarmAfterTime(minAlarm,minute);
     }
 }
-
+// function song Alarm 'mp3 audio'
 function songAlarm(){
     songAlarmMp3.play();
     imgAlarm.classList.add('imgAalarm');
 };
 
 window.onload = function(){
-    listHour.selectedIndex = 0;
-    listMinute.selectedIndex = 0;
-    listAmPm.selectedIndex = 0;
+    [listHour,listMinute,listAmPm].forEach(function(item){
+        item.selectedIndex = 0;
+    });
 }
